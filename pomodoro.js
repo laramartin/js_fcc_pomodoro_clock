54557d6a543670dc59de636f29a57600b3795bda
Lara Martin <larikimc@gmail.com>
2016-05-11 19:36:15 +0200

    comments

--- pomodoro.js
+++ pomodoro.js
@@ -1,11 +1,11 @@
-var timeWork = 1; // minutes
-var timeRest = 1; // minutes
-var time = 0;
-var work = true;
-var started = false;
+var timeWork = 25;    // minutes
+var timeRest = 5;     // minutes
+var time = 0;         // store time for counting
+var work = true;      // if timer is for Work or Break
+var started = false;  // if counter has started
 
 $(document).ready(function() {
-
+  
   // Show time chosen for break and for work
   function displaySetTime(id, input){
     var html = "<p>" + input + "</p>";
@@ -35,6 +35,7 @@ $(document).ready(function() {
     $("#hint").html(html);
   }
 
+  // call all display functions at once
   function display(){
     displaySetTime("timeBreak", timeRest);
     displaySetTime("timeWork", timeWork);
@@ -64,26 +65,38 @@ $(document).ready(function() {
   }
 
   $("button").click(function() {
+    // var to verify if user stops the timer
     var hasStopped = false;
+    // when click a button, if the timer is already running, stops
     if (started){
       started = false;
       hasStopped = true;
       clearInterval(interval);
     }
+    // when clicking a button, check which button
     var val = $(this).attr("value");
+    // add/substract 1 minute to break or work timers
     if (val === "minusBreak"){
       if (timeRest > 0){
         timeRest -= 1;
       }
     } else if (val === "plusBreak"){
-      timeRest += 1;
+      if (timeRest < 59){
+        timeRest += 1;
+      }
     } else if (val === "minusWork"){
       if (timeWork > 0){
         timeWork -= 1;
       }
     } else if (val === "plusWork"){
-      timeWork += 1;
-    } else if (val === "counterButton" && !hasStopped){
+      if (timeWork < 59){
+        timeWork += 1;
+      }
+    }
+    /*  if timer button has been clicked, and wasn't stopped now
+     the timer starts. With hasStopped, I avoid to start the
+     timer when I stop*/
+    else if (val === "counterButton" && !hasStopped){
       if (!started){
         if (work){
           time = timeWork * 60;
@@ -93,7 +106,9 @@ $(document).ready(function() {
         interval = setInterval(timer, 1000);
       }
     }
+    // update UI with every click
     display();
   });
+  // initial display
   display();
 });
