69e9cdf979376bd9b8be1a0b04262afa8b739c56
Lara Martin <larikimc@gmail.com>
2016-05-11 12:51:47 +0200

    buttons functional

--- pomodoro.js
+++ pomodoro.js
@@ -1,3 +1,41 @@
+var timetoWork = 1; // minutes
+var timeToRest = 1; // minutes
+var workLeft = 60; //seconds
+var breakLeft = 60; //seconds
+
 $(document).ready(function() {
 
-}
+  function displaySetTime(id, input){
+    var html = "<p>" + input + "</p>";
+    var idStr = "#".concat(id);
+    $(idStr).html(html);
+  }
+
+  $("button").click(function() {
+    var val = $(this).attr("value");
+    if (val === "minusBreak"){
+      if (timeToRest > 0){
+        timeToRest -= 1;
+        displaySetTime("timeBreak", timeToRest);
+      }
+    }
+    else if (val === "plusBreak"){
+      timeToRest += 1;
+      displaySetTime("timeBreak", timeToRest);
+    }
+    else if (val === "minusWork"){
+      if (timetoWork > 0){
+        timetoWork -= 1;
+        displaySetTime("timeWork", timetoWork);
+      }
+    }
+    else if (val === "plusWork"){
+      timetoWork += 1;
+      displaySetTime("timeWork", timetoWork);
+    }
+  });
+
+
+  displaySetTime("timeBreak", timeToRest);
+  displaySetTime("timeWork", timetoWork);
+});
